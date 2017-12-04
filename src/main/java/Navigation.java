4ab5b6143aa914f858af4ffaf250b716b1826caa
taglyamov <t.aglyamov@yandex.ru>
2017-12-04 15:12:16 +0500

    modify Navigation methods

--- src/main/java/Navigation.java
+++ src/main/java/Navigation.java
@@ -1,22 +1,16 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.interactions.Actions;
-
 
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class Navigation {
     public static void transition(String sectionName) {
-        WebDriver driver = getWebDriver();
-        Actions action= new Actions(driver);
         switch (sectionName) {
             //подвкладки
-            case "Кредитный калькулятор":           action.moveToElement($(By.linkText("Калькуляторы")));action.perform();$(By.linkText("Кредитный калькулятор")).click();    break;
-            case "Интерполятор ставок":             action.moveToElement($(By.linkText("Калькуляторы")));action.perform();$(By.linkText("Интерполятор ставок")).click();      break;
-            case "Верификация внешних ставок":      action.moveToElement($(By.linkText("Загрузка и верификация")));action.perform();$(By.linkText("Верификация внешних ставок")).click();break;
-            case "Верификация внутренних ставок":   action.moveToElement($(By.linkText("Загрузка и верификация")));action.perform();$(By.linkText("Верификация внутренних ставок")).click();break;
-            case "Загрузка ставок":                 action.moveToElement($(By.linkText("Загрузка и верификация")));action.perform();$(By.linkText("Загрузка")).click();       break;
+            case "Кредитный калькулятор":           $(By.linkText("Калькуляторы")).hover();$(By.linkText("Кредитный калькулятор")).click();                                   break;
+            case "Интерполятор ставок":             $(By.linkText("Калькуляторы")).hover();$(By.linkText("Интерполятор ставок")).click();                                     break;
+            case "Верификация внешних ставок":      $(By.linkText("Загрузка и верификация")).hover();$(By.linkText("Верификация внешних ставок")).click();                    break;
+            case "Верификация внутренних ставок":   $(By.linkText("Загрузка и верификация")).hover();$(By.linkText("Верификация внутренних ставок")).click();                 break;
+            case "Загрузка ставок":                 $(By.linkText("Загрузка и верификация")).hover();$(By.linkText("Загрузка")).click();                                      break;
             //вкладки
             case "Расчеты":                         $(By.linkText("Расчеты")).click();                                                                                        break;
             case "Рыночные ставки / Курсы":         $(By.linkText("Рыночные ставки / Курсы")).click();                                                                        break;
